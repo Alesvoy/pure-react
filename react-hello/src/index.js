99693082997e1ff30963bebdaf6378cd9b0ff8e4
Alejandro Butron Perez <alejandro.butron@globant.com>
2021-12-20 09:58:56 -0600

    Finished introduction

---
+++ react-hello/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function HelloWorld() {
+  return (
+    <>
+      <Hello /> <World />!
+    </>
+  );
+}
+
+function Hello() {
+  return <span>Hello</span>;
+}
+
+function World() {
+  return <span>World</span>;
+}
+
+ReactDOM.render(<HelloWorld />, document.querySelector("#root"));
